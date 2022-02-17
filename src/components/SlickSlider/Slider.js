1f764cf76a37c73ec3dacd1e262d84fa4b390a3c
chandrapal Rao <chandrapal@prosperr.io>
2022-02-17 19:54:25 +0530

    tyhis is new commit

--- src/components/SlickSlider/Slider.js
+++ src/components/SlickSlider/Slider.js
@@ -5,113 +5,44 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
 import aa from "../../Assets/Images/image77.png";
-import './SlickSlider.css';
+import "./SlickSlider.css";
 
 export default function SliderBox() {
   const SliderRef = useRef(null);
-  const settings = {
-    // dots: true,
-    // infinite: true,
-    // speed: 5000,
-    centerMode: true,
-    centerPadding: '20px',
-    // slidesToShow: 1,
-    slidesToScroll: 1,
-  }
+
   return (
     <>
-      <div
-        className=""
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "0 10px",
-        }}
-      >
+      <div className=" topHeading">
         <h1> react slider</h1>
-        <div className="" style={{ display: "flex" }}>
+        <div className="Button">
           <div
-            className=""
-            style={{
-              width: 40,
-              height: 40,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              marginRight: 10,
-              boxShadow: "0 1px 3px rgb(0 0 0 / 10%)",
-              borderRadius: 7,
-              cursor: "pointer",
-            }}
+            className="prevButton"
             onClick={() => SliderRef.current.slickPrev()}
           >
             <ArrowBackIos />
           </div>
           <div
-            className=""
-            style={{
-              width: 40,
-              height: 40,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              boxShadow: "0 1px 3px rgb(0 0 0 / 10%)",
-              borderRadius: 7,
-              cursor: "pointer",
-            }}
+            className=" nextButton"
             onClick={() => SliderRef.current.slickNext()}
           >
             <ArrowForwardIos />
           </div>
         </div>
       </div>
-      {/* <
-        className=""
-        style={{
-          // margin: "0 62px 64px" ,
-          padding: "0 72px",
-          border: "1px solid black",
-          // width: "1000px",
-          // display: "flex",
-          // justifyContent:"center",
-          height: "385px",
-          boxSizing: "border-box",
-        }}
-      > */}
-      <div style={{margin:"0 72px", border:"1px solid red"}}>
-         <Slider
-          // dots
-          // dotsClass="slick-dots line-indicator"
-          ref={SliderRef}
-          slidesToShow={4}
-          // slidesToScroll={4}
-        //   customPaging={(i) => (
-        //     <div
-        //       style={{
-        //         position: "absolute",
-        //         width: "100%",
-        //         top: "-10px",
-        //         opacity: 0,
-        //       }}
-        //     >
-        //       {i}
-        //     </div>
-        //   )}
-         >
+
+      <div className="SliderDiv">
+        <Slider ref={SliderRef} slidesToShow={4}>
           {Array(10)
             .fill("")
             .map(() => (
-              <div style={{  }}>
-                 <div style={{ width: 268, height:320, margin:"0 20 0 0 ", border:"1px solid red",borderRadius: 10 }}>
-                   <img className="AdvisorIMG"
-                  style={{ width: 268, height:182,borderRadius: 10  }}
-src={aa}                  alt=""
-                />  chandrapal rao</div>
+              <div>
+                <div className="AdvisorCard">
+                  <img className="AdvisorIMG" src={aa} alt="" /> chandrapal rao
+                </div>
               </div>
             ))}
-        </Slider></div>
-      
+        </Slider>
+      </div>
     </>
   );
 }
