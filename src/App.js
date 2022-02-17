52fc193d844459e6e840c02063d9c9b933b086f4
chandrapal Rao <chandrapal@prosperr.io>
2022-02-17 18:50:14 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
    
   return (
     <>
-   <TopArticleBox/>
-   <AdvisorProfile/>
+   {/* <TopArticleBox/>
+   <AdvisorProfile/> */}
    {/* <PatternValidation/> */}
    {/* <Fetch/> */}
    <SliderBox/>
